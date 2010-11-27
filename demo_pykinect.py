96e3066abd48d6575d167c34eeda34931b8c9d8f
Andrew <amiller@dappervision.com>
2010-11-26 20:08:15 -0500

    Initial commit

---
+++ demo_pykinect.py
@@ -0,0 +1,271 @@
+"""
+This is a grand unifying demo of the python scientific computing environments. It lets you
+tinker with kinect data in OpenCV, OpenGL, and Matplotlib, all at the same time!
+
+It requires IPython, Matplotlib, OpenCV 2.1, PyOpengl (and possibly others) to run.
+You also need to build the cython_sync library:
+  cd cython_sync
+  python setup.py build_ext -b ../
+
+
+Usage instructions
+-------------------
+
+0. Please run this script using:
+  ipython -pylab -wthread demo_pykinect.py
+
+1. You should see an opengl window pop up with a preview of a point cloud. You can pan and 
+  zoom with the mouse. Run the following commands:
+  - update()      # Grabs one frame from kinect and updates the point cloud
+  - update_on()   # Grabs frames from kinect on a thread - 3d Video! (might be slow!)
+  - update_off()  # Stops the update thread
+  
+2. You can also use opencv:
+  - loopcv()      # Grab frames and display them as a cv image
+    (ctrl+c to break)
+  
+3. You can also use matplotlib:
+  - imshow(depth)
+  
+4. Most importantly, you can reload any of the code without pausing or destroying your 
+  python instance:
+  - %run -i demo_pykinect.py
+  
+  Try changing some of the code, like the downsampling factor (search: downsampling)
+  or the point size (search: GL_POINT_SIZE) and update the code without quitting python.
+
+This is an ideal environment for developing 3D point cloud algorithms and visualizations.
+All of your tools are right at hand. 
+
+Note to MATLAB users: 
+  Yes, MATLAB already does most of this... there are plenty of reasons to prefer python,
+  one of which is access to OpenGL drawing commands - scatter3 isn't adequate!
+"""
+
+
+import pykinectwindow as wxwindow
+import numpy as np
+import pylab
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import time
+import freenect
+
+
+# I probably need more help with these!
+try: 
+  TEXTURE_TARGET = GL_TEXTURE_RECTANGLE
+except:
+  TEXTURE_TARGET = GL_TEXTURE_RECTANGLE_ARB
+
+
+if not 'win' in globals(): win = wxwindow.Window(size=(640,480))
+
+def refresh(): win.Refresh()
+
+if not 'rotangles' in globals(): rotangles = [0,0]
+if not 'zoomdist' in globals(): zoomdist = 1
+if not 'projpts' in globals(): projpts = (None, None)
+if not 'rgb' in globals(): rgb = None
+
+def create_texture():
+  global rgbtex
+  rgbtex = glGenTextures(1)
+  glBindTexture(TEXTURE_TARGET, rgbtex)
+  glTexImage2D(TEXTURE_TARGET,0,GL_RGB,640,480,0,GL_RGB,GL_UNSIGNED_BYTE,None)
+
+
+if not '_mpos' in globals(): _mpos = None
+@win.eventx
+def EVT_LEFT_DOWN(event):
+  global _mpos
+  _mpos = event.Position
+  
+@win.eventx
+def EVT_LEFT_UP(event):
+  global _mpos
+  _mpos = None
+  
+@win.eventx
+def EVT_MOTION(event):
+  global _mpos
+  if event.LeftIsDown():
+    if _mpos:
+      (x,y),(mx,my) = event.Position,_mpos
+      rotangles[0] += y-my
+      rotangles[1] += x-mx
+      refresh()    
+    _mpos = event.Position
+
+
+@win.eventx
+def EVT_MOUSEWHEEL(event):
+  global zoomdist
+  dy = event.WheelRotation
+  zoomdist *= np.power(0.95, -dy)
+  refresh()
+  
+
+# Return a point cloud, an Nx3 array, made by projecting the kinet depth map 
+# through calibration / registration
+# u, v are image coordinates, depth comes from the kinect
+def project(depth, u, v):
+  Z = 1.0 / (-0.0030711*depth.flatten() + 3.3309495)
+  X = (u.flatten() - 320.0) * Z / 590.0
+  Y = (v.flatten() - 240.0) * Z / 590.0
+  U = u.flatten()
+  V = v.flatten()
+  xyz = np.vstack((X,-Y,-Z)).transpose()
+  uv = np.vstack((U,V)).transpose()
+  xyz = xyz[Z>0,:]
+  uv = uv[Z>0,:]
+  return xyz, uv
+
+clearcolor = [0,0,0,0]
+@win.event
+def on_draw():  
+  if not 'rgbtex' in globals():
+    create_texture()
+
+  xyz, uv = projpts
+  if xyz is None: return
+
+  if not rgb is None:
+    rgb_ = (rgb.astype(np.float32) * 4 + 70).clip(0,255).astype(np.uint8)
+    glBindTexture(TEXTURE_TARGET, rgbtex)
+    glTexSubImage2D(TEXTURE_TARGET, 0, 0, 0, 640, 480, GL_RGB, GL_UNSIGNED_BYTE, rgb_);
+
+  glClearColor(*clearcolor)
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+  glEnable(GL_DEPTH_TEST)
+
+  # flush that stack in case it's broken from earlier
+  glPushMatrix()
+
+  glMatrixMode(GL_PROJECTION)
+  glLoadIdentity()
+  gluPerspective(60, 4/3., 0.3, 200)
+
+  glMatrixMode(GL_MODELVIEW)
+  glLoadIdentity()
+
+  def mouse_rotate(xAngle, yAngle, zAngle):
+    glRotatef(xAngle, 1.0, 0.0, 0.0);
+    glRotatef(yAngle, 0.0, 1.0, 0.0);
+    glRotatef(zAngle, 0.0, 0.0, 1.0);
+  glScale(zoomdist,zoomdist,1)
+  glTranslate(0, 0,-1.5)
+  mouse_rotate(rotangles[0], rotangles[1], 0);
+  glTranslate(0,0,1.5)
+  #glTranslate(0, 0,-1)
+
+  # Draw some axes
+  if 0:
+    glBegin(GL_LINES)
+    glColor3f(1,0,0); glVertex3f(0,0,0); glVertex3f(1,0,0)
+    glColor3f(0,1,0); glVertex3f(0,0,0); glVertex3f(0,1,0)
+    glColor3f(0,0,1); glVertex3f(0,0,0); glVertex3f(0,0,1)
+    glEnd()
+
+
+  # Draw the points
+  glPointSize(2)
+  glEnableClientState(GL_VERTEX_ARRAY)
+  glEnableClientState(GL_TEXTURE_COORD_ARRAY)
+  glVertexPointerf(xyz)
+  glTexCoordPointerf(uv)
+  glEnable(TEXTURE_TARGET)
+  glColor3f(1,1,1)
+  glDrawElementsui(GL_POINTS, np.array(range(len(xyz))))
+  glDisableClientState(GL_VERTEX_ARRAY)
+  glDisableClientState(GL_TEXTURE_COORD_ARRAY)
+  glDisable(TEXTURE_TARGET)
+
+  #
+  if 0:
+      inds = np.nonzero(xyz[:,2]>-0.55)
+      glPointSize(10)
+      glColor3f(0,1,1)
+      glEnableClientState(GL_VERTEX_ARRAY)
+      glDrawElementsui(GL_POINTS, np.array(inds))
+      glDisableClientState(GL_VERTEX_ARRAY)
+
+  if 0:
+      # Draw only the points in the near plane
+      glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA)
+      glEnable(GL_BLEND)
+      glColor(0.9,0.9,1.0,0.8)
+      glPushMatrix()
+      glTranslate(0,0,-0.55)
+      glScale(0.6,0.6,1)
+      glBegin(GL_QUADS)
+      glVertex3f(-1,-1,0); glVertex3f( 1,-1,0);
+      glVertex3f( 1, 1,0); glVertex3f(-1, 1,0);
+      glEnd()
+      glPopMatrix()
+      glDisable(GL_BLEND)
+
+  glPopMatrix()
+
+
+# A silly loop that shows you can busy the ipython thread while opengl runs
+def playcolors():
+  while 1:
+    global clearcolor
+    clearcolor = [np.random.random(),0,0,0]
+    time.sleep(0.1)
+    refresh()
+
+# Update the point cloud from the shell or from a background thread!
+
+def update(dt=0):
+  global projpts, rgb, depth
+  depth,_ = freenect.sync_get_depth_np()
+  rgb,_ = freenect.sync_get_rgb_np()
+  q = depth
+  X,Y = np.meshgrid(range(640),range(480))
+  # YOU CAN CHANGE THIS AND RERUN THE PROGRAM!
+  # Point cloud downsampling
+  d = 4
+  projpts = project(q[::d,::d],X[::d,::d],Y[::d,::d])
+  refresh()
+  
+def update_join():
+  update_on()
+  try:
+    _thread.join()
+  except:
+    update_off()
+  
+def update_on():
+  global _updating
+  if not '_updating' in globals(): _updating = False
+  if _updating: return
+  
+  _updating = True
+  from threading import Thread
+  global _thread
+  def _run():
+    while _updating:
+      update()
+  _thread = Thread(target=_run)
+  _thread.start()
+  
+def update_off():
+  global _updating
+  _updating = False
+  
+  
+# Get frames in a loop and display with opencv
+def loopcv():
+  import cv
+  while 1:
+    cv.ShowImage('hi',get_depth_np().astype(np.uint8))
+    cv.WaitKey(10)
+
+update() 
+#update_on()
+
+
+
+
---
+++ pykinectwindow.py
@@ -0,0 +1,80 @@
+# This module requires IPython to work! It is meant to be used from an IPython environment with: 
+#   ipython -wthread and -pylab
+# See demo_pykinect.py for an example
+
+import wx
+from wx import glcanvas
+from OpenGL.GL import *
+
+# Get the app ourselves so we can attach it to each window
+if not '__myapp' in wx.__dict__:
+  wx.__myapp = wx.PySimpleApp()
+app = wx.__myapp
+
+class Window(wx.Frame):
+  
+    # wx events can be put in directly
+    def eventx(self, target):
+        def wrapper(*args, **kwargs):
+          target(*args, **kwargs)
+        self.canvas.Bind(wx.__dict__[target.__name__], wrapper)
+  
+    # Events special to this class, just add them this way
+    def event(self, target):
+        def wrapper(*args, **kwargs):
+          target(*args, **kwargs)   
+        self.__dict__[target.__name__] = wrapper
+        
+    def _wrap(self, name, *args, **kwargs):
+      try:
+        self.__getattribute__(name)
+      except KeyError:
+        pass
+      else:
+        self.__getattribute__(name)(*args, **kwargs)
+            
+
+    def __init__(self, title='WxWindow', id=-1, pos=wx.DefaultPosition,
+                 size=wx.DefaultSize, style=wx.DEFAULT_FRAME_STYLE,
+                 name='frame'):
+                 
+        style = wx.DEFAULT_FRAME_STYLE | wx.NO_FULL_REPAINT_ON_RESIZE
+
+        super(Window,self).__init__(None, id, title, pos, size, style, name)
+
+        attribList = (glcanvas.WX_GL_RGBA, # RGBA
+                      glcanvas.WX_GL_DOUBLEBUFFER, # Double Buffered
+                      glcanvas.WX_GL_DEPTH_SIZE, 24) # 24 bit
+              
+        self.canvas = glcanvas.GLCanvas(self, attribList=attribList)
+
+        self.canvas.Bind(wx.EVT_ERASE_BACKGROUND, self.processEraseBackgroundEvent)
+        self.canvas.Bind(wx.EVT_SIZE, self.processSizeEvent)
+        self.canvas.Bind(wx.EVT_PAINT, self.processPaintEvent)
+        self.Show()
+
+    def processEraseBackgroundEvent(self, event):
+        """Process the erase background event."""
+        pass # Do nothing, to avoid flashing on MSWin
+
+    def processSizeEvent(self, event):
+        """Process the resize event."""
+        if self.canvas.GetContext():
+            # Make sure the frame is shown before calling SetCurrent.
+            #self.Show()
+            self.canvas.SetCurrent()
+            size = self.GetClientSize()
+            self.OnReshape(size.width, size.height)
+            #self.canvas.Refresh(False)
+        event.Skip()
+
+    def processPaintEvent(self, event=None):
+        """Process the drawing event."""
+        self.canvas.SetCurrent()
+        self._wrap('on_draw')
+        self.canvas.SwapBuffers()
+        if event: event.Skip()
+
+    def OnReshape(self, width, height):
+        """Reshape the OpenGL viewport based on the dimensions of the window."""
+        glViewport(0, 0, width, height)
