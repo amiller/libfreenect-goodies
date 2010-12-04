c5c96f8c4be5f3914a7e141467420f5d70dcd058
Andrew <amiller@dappervision.com>
2010-12-04 18:57:53 -0500

    Updated demo_pykinect and brought the projection functions into calibkinct
    Conflicts:
    
    	demo_pykinect.py
    
    Signed-off-by: Andrew <amiller@dappervision.com>

--- demo_pykinect.py
+++ demo_pykinect.py
@@ -5,6 +5,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 import time
 import freenect
+import calibkinect
 
 
 # I probably need more help with these!
@@ -61,56 +62,6 @@ def EVT_MOUSEWHEEL(event):
   refresh()
   
 
-# Return a point cloud, an Nx3 array, made by projecting the kinet depth map 
-# through calibration / registration
-# u, v are image coordinates, depth comes from the kinect
-def project(depth, u, v):
-  C = np.vstack((u.flatten(), v.flatten(), depth.flatten(), 0*u.flatten()+1))
-  
-  # Project the vertices using xyz_matrix()
-  X,Y,Z,W = np.dot(xyz_matrix(),C)
-  X,Y,Z = X/W, Y/W, Z/W
-  xyz = np.vstack((X,Y,Z)).transpose()
-  xyz = xyz[Z<0,:]
-  
-  # Project the color coordinates using rgb_matrix() and xyz_matrix()
-  U,V,_,W = np.dot(np.dot(rgb_matrix(), xyz_matrix()),C)
-  U,V = U/W, V/W
-  uv = np.vstack((U,V)).transpose()    
-  uv = uv[Z<0,:]       
-  
-  return xyz, uv
-<<<<<<< HEAD:demo_pykinect.py
-=======
-  
-def rgb_matrix():
-  rot = np.array([[ 9.99846e-01,   -1.26353e-03,   1.74872e-02], 
-                  [-1.4779096e-03, -9.999238e-01,  1.225138e-02],
-                  [1.747042e-02,   -1.227534e-02,  -9.99772e-01]])
-  trans = np.array([[1.9985e-02, -7.44237e-04,-1.0916736e-02]])
-  m = np.hstack((rot, -trans.transpose()))
-  m = np.vstack((m, np.array([[0,0,0,1]])))
-  KK = np.array([[529.2, 0, 329, 0],
-                 [0, 525.6, 267.5, 0],
-                 [0, 0, 0, 0],
-                 [0, 0, 1, 0]])
-  m = np.dot(KK, (m))
-  return m.astype(np.float32)
-  
-def xyz_matrix():
-  fx = 594.21
-  fy = 591.04
-  a = -0.0030711
-  b = 3.3309495
-  cx = 339.5
-  cy = 242.7
-  mat = np.array([[1/fx, 0, 0, -cx/fx],
-                  [0, -1/fy, 0, cy/fy],
-                  [0,   0, 0,    -1],
-                  [0,   0, a,     b]])
-  return mat.astype(np.float32)
->>>>>>> 5365c9c... Updated the pykinect demo to include some calibration matrices:demo_pykinect.py
-
 clearcolor = [0,0,0,0]
 @win.event
 def on_draw():  
@@ -144,7 +95,7 @@ def on_draw():
     glRotatef(yAngle, 0.0, 1.0, 0.0);
     glRotatef(zAngle, 0.0, 0.0, 1.0);
   glScale(zoomdist,zoomdist,1)
-  glTranslate(0, 0,-1.5)
+  glTranslate(0, 0,-3.5)
   mouse_rotate(rotangles[0], rotangles[1], 0);
   glTranslate(0,0,1.5)
   #glTranslate(0, 0,-1)
@@ -157,8 +108,6 @@ def on_draw():
     glColor3f(0,0,1); glVertex3f(0,0,0); glVertex3f(0,0,1)
     glEnd()
 
-<<<<<<< HEAD:demo_pykinect.py
-=======
   # We can either project the points ourselves, or embed it in the opengl matrix
   if 0:
     dec = 4
@@ -170,13 +119,13 @@ def on_draw():
     
     glMatrixMode(GL_TEXTURE)
     glLoadIdentity()
-    glMultMatrixf(rgb_matrix().transpose())
-    glMultMatrixf(xyz_matrix().transpose())
+    glMultMatrixf(calibkinect.uv_matrix().transpose())
+    glMultMatrixf(calibkinect.xyz_matrix().transpose())
     glTexCoordPointers(np.array(points))
     
     glMatrixMode(GL_MODELVIEW)
     glPushMatrix()
-    glMultMatrixf(xyz_matrix().transpose())
+    glMultMatrixf(calibkinect.xyz_matrix().transpose())
     glVertexPointers(np.array(points))
   else:
     glMatrixMode(GL_TEXTURE)
@@ -185,26 +134,18 @@ def on_draw():
     glPushMatrix()
     glVertexPointerf(xyz)
     glTexCoordPointerf(uv)
->>>>>>> 5365c9c... Updated the pykinect demo to include some calibration matrices:demo_pykinect.py
 
   # Draw the points
   glPointSize(2)
   glEnableClientState(GL_VERTEX_ARRAY)
   glEnableClientState(GL_TEXTURE_COORD_ARRAY)
-<<<<<<< HEAD:demo_pykinect.py
-  glVertexPointerf(xyz)
-  glTexCoordPointerf(uv)
-  glEnable(TEXTURE_TARGET)
-  glColor3f(1,1,1)
-  glDrawElementsui(GL_POINTS, np.array(range(len(xyz))))
-=======
   glEnable(TEXTURE_TARGET)
   glColor3f(1,1,1)
   glDrawElementsui(GL_POINTS, np.array(range(xyz.shape[0])))
->>>>>>> 5365c9c... Updated the pykinect demo to include some calibration matrices:demo_pykinect.py
   glDisableClientState(GL_VERTEX_ARRAY)
   glDisableClientState(GL_TEXTURE_COORD_ARRAY)
   glDisable(TEXTURE_TARGET)
+  glPopMatrix()
 
   #
   if 0:
@@ -246,13 +187,13 @@ def playcolors():
 def update(dt=0):
   global projpts, rgb, depth
   depth,_ = freenect.sync_get_depth()
-  rgb,_ = freenect.sync_get_rgb()
+  rgb,_ = freenect.sync_get_video()
   q = depth
   X,Y = np.meshgrid(range(640),range(480))
   # YOU CAN CHANGE THIS AND RERUN THE PROGRAM!
   # Point cloud downsampling
   d = 4
-  projpts = project(q[::d,::d],X[::d,::d],Y[::d,::d])
+  projpts = calibkinect.depth2xyzuv(q[::d,::d],X[::d,::d],Y[::d,::d])
   refresh()
   
 def update_join():
